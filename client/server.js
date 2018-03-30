50184114ace0d8ca82222cdbd4f61943ca71e016
HowardTao <HowardTao@users.noreply.github.com>
2018-03-30 10:35:46 +0800

    Initial commit

---
+++ client/server.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config),{
+    publicPath:config.output.publicPath,
+    hot:false,
+    historyApiFallback:true
+}).listen(3000,"localhost",function(err,result){
+    if(err){
+        console.log(err);
+    }
+    console.log("Listening at loaclhost:3000")
+});
